bdea9cbb671c5ff66ce3db2b1d0571eeb3f64435
Ronald Thomas <ronaldthomas094@gmail.com>
2024-10-08 18:29:28 +1100

    meetup id data from mongodb

--- pages/[meetupid]/index.js
+++ pages/[meetupid]/index.js
@@ -1,32 +1,38 @@
 import React from "react";
+import { MongoClient, ObjectId } from "mongodb";
 import MeetupDetail from "../../components/meetups/MeetupDetail";
 
-const MeetupDetailsPage = (meetupData) => {
+const MeetupDetailsPage = (props) => {
+  const { meetupData } = props;
   return (
     <MeetupDetail
-      image="https://upload.wikimedia.org/wikipedia/commons/a/a0/OfferUp_Community_MeetUp_Spot_Sign.png"
-      title="First meetup"
-      address="123 Sydney Australia"
-      description="Meetup description"
+      image={meetupData.image}
+      title={meetupData.title}
+      address={meetupData.address}
+      description={meetupData.description}
     />
   );
 };
 
 export async function getStaticPaths() {
+  const client = await MongoClient.connect(
+    "mongodb+srv://ronaldthomas094db:irzAh9W5WriXKXN1@ronaldmongocluster.0ronf.mongodb.net/meetups?retryWrites=true&w=majority&appName=ronaldmongocluster"
+  );
+
+  const db = client.db();
+
+  const meetupsCollections = db.collection("meetups");
+
+  const meetups = await meetupsCollections.find({}, { _id: 1 }).toArray();
+  console.log("meetups", meetups);
+
+  client.close();
+
   return {
     fallback: false,
-    paths: [
-      {
-        params: {
-          meetupid: "m1",
-        },
-      },
-      {
-        params: {
-          meetupid: "m2",
-        },
-      },
-    ],
+    paths: meetups.map((meetup) => ({
+      params: { meetupid: meetup._id.toString() },
+    })),
   };
 }
 
@@ -34,17 +40,30 @@ export async function getStaticProps(context) {
   // Fetch data for API
   const meetupId = context.params.meetupid;
 
-  console.log("meetupId", meetupId);
+  const client = await MongoClient.connect(
+    "mongodb+srv://ronaldthomas094db:irzAh9W5WriXKXN1@ronaldmongocluster.0ronf.mongodb.net/meetups?retryWrites=true&w=majority&appName=ronaldmongocluster"
+  );
+
+  const db = client.db();
+
+  const meetupsCollections = db.collection("meetups");
+
+  const selectedMeetup = await meetupsCollections.findOne({
+    _id: new ObjectId(meetupId),
+  });
+
+  console.log("selectedMeetup", selectedMeetup);
+
+  client.close();
 
   return {
     props: {
       meetupData: {
-        image:
-          "https://upload.wikimedia.org/wikipedia/commons/a/a0/OfferUp_Community_MeetUp_Spot_Sign.png",
-        title: "First meetup",
-        address: "123 Sydney Australia",
-        description: "Meetup description",
-        id: meetupId,
+        id: selectedMeetup._id.toString(),
+        title: selectedMeetup.title,
+        address: selectedMeetup.address,
+        description: selectedMeetup.description,
+        image: selectedMeetup.image,
       },
     },
   };
